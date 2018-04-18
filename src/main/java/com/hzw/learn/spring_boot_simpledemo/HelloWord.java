46acc6f5b45ab3c265517b73bc7fae4b9ea9866e
侯正伟 <houzw@allinfinance.com>
2018-04-18 12:03:10 +0800

    env变量测试

--- src/main/java/com/hzw/learn/spring_boot_simpledemo/HelloWord.java
+++ src/main/java/com/hzw/learn/spring_boot_simpledemo/HelloWord.java
@@ -1,20 +1,30 @@
 package com.hzw.learn.spring_boot_simpledemo;
 
 import java.text.ParseException;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("helloword")
+@RequestMapping("")
 public class HelloWord{
 	
+	@Autowired
+	private EnvValue envValue;
+	
 	@ResponseBody
-	@RequestMapping("/index")
+	@RequestMapping("/*")
 	public String index() throws ParseException {
-		System.out.println("hello houzw");
-		return "hello houzw";
+		
+		Object enva = System.getenv("env-a");
+		Map<String, String> envMaps = envValue.getEnvmaps();
+		System.out.println("get value from systemenv whith 'env-a':" + enva);
+		System.out.println("envMaps:" + envMaps);
+		
+		return envMaps.toString();
 	}
 	
 	
